98706505af9427430edd2ba83389c770f91561f1
Akash-Patil-12 <patilakash121212@gmail.com>
2021-06-30 20:58:26 +0530

    Added : Calculate Employee Wage is added

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 
 empCheck=$((RANDOM%2))
-
+empWagePerHr=20
 if [ $empCheck -eq 1 ]
 then
   echo Employee is present
+  echo "employee wage is :$(($empWagePerHr*8))"
 else
   echo Employee is absent
+  echo "employee wage is :$(($empWagePerHr*0))"
 fi
